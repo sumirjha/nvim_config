4a960b27f9ebf90b2d80d0076224d26830b832c4
Sumir.Jha <sumir.jha@adder.com>
2024-08-22 14:43:40 +0100

    Added LSP and Telescope.

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -67,7 +67,11 @@ return {
 			
 			-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-			
+
+			lspconfig.cmake.setup({
+				capabilities = capabilities,
+			})
+
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
 			})
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,11 +1,17 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-live-grep-args.nvim'
+	},
     config = function ()
     	local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
+		vim.keymap.set("n", "<leader>ft", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+
+		require("telescope").load_extension("live_grep_args")
     end,
 }
